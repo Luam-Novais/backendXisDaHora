2a549d6a4e38233066363c60c6c3933f5165f99f
Luam-Novais <luampabl014@gmail.com>
2025-06-16 22:52:24 -0300

    feat:add .env and change database

--- models/db.js
+++ models/db.js
@@ -1,17 +1,16 @@
+require('dotenv').config()
+
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize("railway", "root", "sObAoSNiJRwBOZZEVFiZgrszCrazDlqy", {
-
-    host: "ballast.proxy.rlwy.net",
-    port: 37245,
-    dialect: "mysql"
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+     dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
 })
 
-// const sequelize = new Sequelize("xisdahora", "root", "root", {
-
-//     host: "localhost",
-//     dialect: "mysql"
-// })
 
 sequelize.authenticate()
     .then(() => console.log("Conectado ao banco de dados"))
