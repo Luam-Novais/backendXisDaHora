648dbcfbf995b035d0bf7e38efe042f1b032e518
Luam-Novais <luampabl014@gmail.com>
2025-06-17 18:54:11 -0300

    fix: ensure SSL requirement for database connection

--- models/db.js
+++ models/db.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
      dialectOptions: {
       ssl: {
+        require: true,
         rejectUnauthorized: false,
       },
     }
