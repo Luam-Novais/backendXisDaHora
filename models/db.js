00739f3f6c4e49201a01cfb1ef8d218684799f0c
Luam-Novais <luampabl014@gmail.com>
2025-06-17 21:14:13 -0300

    fix: update Sequelize connection string to use hardcoded database URL

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize("postgresql://xis-da-hora-db_owner:npg_pjY4JXztEF1w@ep-damp-wildflower-acjs20qd-pooler.sa-east-1.aws.neon.tech/xis-da-hora-db?sslmode=require", {
     dialect: "postgres",
      dialectOptions: {
       ssl: {
