cf3c72b9dbcf2e063de8c7d73114835bbd6dd244
Luam-Novais <luampabl014@gmail.com>
2025-06-17 21:52:54 -0300

    fix: update DATABASE_URL to new neon connection string

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize("postgresql://postgres:Luampabl014!@db.szxieyvbgmyhvbzakgev.supabase.co:5432/postgres", {
+const sequelize = new Sequelize("postgresql://xis-da-hora-db_owner:npg_pjY4JXztEF1w@ep-damp-wildflower-acjs20qd-pooler.sa-east-1.aws.neon.tech/xis-da-hora-db?sslmode=require", {
     dialect: "postgres",
      dialectOptions: {
       ssl: {
